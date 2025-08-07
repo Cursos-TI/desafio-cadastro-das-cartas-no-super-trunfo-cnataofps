165affda112b30b5abc3dbe6c473e7bda57ac6f7
Lucas Augusto <lucas_mtv15@yahoo.com.br>
2025-08-07 16:00:00 +0000

    correção feito conforme o solicitado pela IA

--- super_trunfo.c
+++ super_trunfo.c
@@ -19,6 +19,7 @@ int main() {
     float area, pib, densidadepopulacional, pibpercapita;
     double SuperPoder;
     int temp_char;
+    unsigned int pop_venceu_c1, area_venceu_c1, pib_venceu_c1, pontos_venceu_c1, dens_venceu_c1, pib_pc_venceu_c1, super_poder_venceu_c1;
     
     
     // Variaveis para segunda carta 
@@ -28,6 +29,7 @@ int main() {
     unsigned long int populacao2;
     float area2, pib2, densidadepopulacional2, pibpercapita2;
     double SuperPoder2;
+    unsigned int pop_venceu_c2, area_venceu_c2, pib_venceu_c2, pontos_venceu_c2, dens_venceu_c2, pib_pc_venceu_c2, super_poder_venceu_c2;
     
     
 
@@ -150,63 +152,68 @@ int main() {
     // Calcular o Super Poder CARTA 2
 
    if (densidadepopulacional2 != 0) {        
-        SuperPoder2 = (double)populacao2 + area2 + pib2 + (double)pontoTuristico2 + pibpercapita2 + (1.0 / densidadepopulacional);    
+        SuperPoder2 = (double)populacao2 + area2 + pib2 + (double)pontoTuristico2 + pibpercapita2 + (1.0 / densidadepopulacional2);    
     } else {
         SuperPoder2 = 0.0;       
         printf("Atenção: Densidade populacional da Carta 2 é zero. Super Poder pode ser afetado.\n");    
     }
 
-    // Comparação das cartas 1 e 2 
+    // Comparação das cartas 1 
 
-    ResultadoCarta = populacao > populacao2;
-    ResultadoCarta = area > area2;
-    ResultadoCarta = pib > pib2;
-    ResultadoCarta = pontoTuristico > pontoTuristico2;
-    ResultadoCarta = densidadepopulacional < densidadepopulacional2;
-    ResultadoCarta = pibpercapita > pibpercapita2;
-    ResultadoCarta = SuperPoder > SuperPoder2;
+    pop_venceu_c1 = populacao > populacao2;
+    area_venceu_c1 = area > area2;
+    pib_venceu_c1 = pib > pib2;
+    pontos_venceu_c1 = pontoTuristico > pontoTuristico2;
+    dens_venceu_c1 = densidadepopulacional < densidadepopulacional2;
+    pib_pc_venceu_c1 = pibpercapita > pibpercapita2;
+    super_poder_venceu_c1 = SuperPoder > SuperPoder2;
 
-    ResultadoCarta2 = populacao2 > populacao;
-    ResultadoCarta2 = area2 > area;
-    ResultadoCarta2 = pib2 > pib;
-    ResultadoCarta2 = pontoTuristico2 > pontoTuristico;
-    ResultadoCarta2 = densidadepopulacional2 < densidadepopulacional;
-    ResultadoCarta2 = pibpercapita2 > pibpercapita;
-    ResultadoCarta2 = SuperPoder2 > SuperPoder;
+    // Comparação das cartas 2
+
+    pop_venceu_c2 = populacao2 > populacao;
+    area_venceu_c2 = area2 > area;
+    pib_venceu_c2 = pib2 > pib;
+    pontos_venceu_c2 = pontoTuristico2 > pontoTuristico;
+    dens_venceu_c2 = densidadepopulacional2 < densidadepopulacional;
+    pib_pc_venceu_c2 = pibpercapita2 > pibpercapita;
+    super_poder_venceu_c2 = SuperPoder2 > SuperPoder;
 
 
     // Exibição dos Resultados das comparaçoes da Carta 1 
+    // Resultado '1' geralmente significa vence e '0' significa Derrota/empate
 
     printf("\n\n \033[1;34m Informações_Da_Carta 1: \033[0m \n\n");
 
-    printf("Estado: \033[1;34m%c\033[0m(%u)\n", estado, ResultadoCarta);
-    printf("Código: \033[1;34m%s\033[0m(%u)\n", codigo, ResultadoCarta);
-    printf("Cidade: \033[1;34m%s\033[0m(%u)\n", nomeCidade, ResultadoCarta);
-    printf("População: \033[1;34m%lu'\033[0m(%u)\n", populacao, ResultadoCarta);
-    printf("Área: \033[1;34m%'.2fkm²\033[0m(%u)\n", area, ResultadoCarta);
-    printf("PIB: \033[1;34m%'.2f Bi\033[0m (%u)\n", pib, ResultadoCarta);
-    printf("Pontos Turísticos: \033[1;34m%u Lugares\033[0m(%u)\n", pontoTuristico, ResultadoCarta);
-    printf("Densidade Populacional: \033[1;34m%'.2f Hab/km²\033[0m(%u)\n", densidadepopulacional, ResultadoCarta);
-    printf("PIB per Capita: \033[1;34m%'.2f R$\033[0m(%u)\n", pibpercapita, ResultadoCarta);
-    printf("Super Poder: \033[1;33m%.2lf\033[0m(%u)\n\n", SuperPoder, ResultadoCarta);
+    printf("Estado: \033[1;34m%c\033[0m(%u)\n", estado);
+    printf("Código: \033[1;34m%s\033[0m(%u)\n", codigo);
+    printf("Cidade: \033[1;34m%s\033[0m(%u)\n", nomeCidade);
+    printf("População: \033[1;34m%lu'\033[0m(%u)\n", populacao, pop_venceu_c1);
+    printf("Área: \033[1;34m%'.2fkm²\033[0m(%u)\n", area, area_venceu_c1);
+    printf("PIB: \033[1;34m%'.2f Bi\033[0m (%u)\n", pib, pib_venceu_c1);
+    printf("Pontos Turísticos: \033[1;34m%u Lugares\033[0m(%u)\n", pontoTuristico, pontos_venceu_c1);
+    printf("Densidade Populacional: \033[1;34m%'.2f Hab/km²\033[0m(%u)\n", densidadepopulacional, dens_venceu_c1);
+    printf("PIB per Capita: \033[1;34m%'.2f R$\033[0m(%u)\n", pibpercapita, pib_pc_venceu_c1);
+    printf("Super Poder: \033[1;33m%.2lf\033[0m(%u)\n\n", SuperPoder, super_poder_venceu_c1);
 
     
 
    // Exibição dos Resultados das comparaçoes da Carta 2
+   // Resultado '1' geralmente significa vence e '0' significa Derrota/empate
 
     printf("\n\n \033[1;31m Informações_Da_Carta 2: \033[0m \n\n");
 
-    printf("Estado: \033[1;31m%c\033[0m(%u)\n", estado2, ResultadoCarta);
-    printf("Código: \033[1;31m%s\033[0m(%u)\n", codigo2, ResultadoCarta);
-    printf("Cidade: \033[1;31m%s\033[0m(%u)\n", nomeCidade2, ResultadoCarta);
-    printf("População: \033[1;31m%lu'\033[0m(%u)\n", populacao2, ResultadoCarta);
-    printf("Área: \033[1;31m%'.2fkm²\033[0m(%u)\n", area2, ResultadoCarta);
-    printf("PIB: \033[1;31m%'.2f Bi\033[0m(%u)\n", pib2, ResultadoCarta);
-    printf("Pontos Turísticos: \033[1;31m%u Lugares\033[0m(%u)\n", pontoTuristico2, ResultadoCarta);
-    printf("Densidade Populacional: \033[1;31m%'.2f Hab/km²\033[0m(%u)\n", densidadepopulacional2, ResultadoCarta);
-    printf("PIB per Capita: \033[1;31m%'.2f R$\033[0m(%u)\n", pibpercapita2, ResultadoCarta);
-    printf("Super Poder: \033[1;33m%.2lf\033[0m(%u)\n\n", SuperPoder2, ResultadoCarta);   
-    
+    printf("Estado: \033[1;34m%c\033[0m(%u)\n", estado2);
+    printf("Código: \033[1;34m%s\033[0m(%u)\n", codigo2);
+    printf("Cidade: \033[1;34m%s\033[0m(%u)\n", nomeCidade2);
+    printf("População: \033[1;34m%lu'\033[0m(%u)\n", populacao2, pop_venceu_c2);
+    printf("Área: \033[1;34m%'.2fkm²\033[0m(%u)\n", area2, area_venceu_c2);
+    printf("PIB: \033[1;34m%'.2f Bi\033[0m (%u)\n", pib2, pib_venceu_c2);
+    printf("Pontos Turísticos: \033[1;34m%u Lugares\033[0m(%u)\n", pontoTuristico2, pontos_venceu_c2);
+    printf("Densidade Populacional: \033[1;34m%'.2f Hab/km²\033[0m(%u)\n", densidadepopulacional2, dens_venceu_c2);
+    printf("PIB per Capita: \033[1;34m%'.2f R$\033[0m(%u)\n", pibpercapita2, pib_pc_venceu_c2);
+    printf("Super Poder: \033[1;33m%.2lf\033[0m(%u)\n\n", SuperPoder2, super_poder_venceu_c2);
+
+       
 
     return 0;
 }
